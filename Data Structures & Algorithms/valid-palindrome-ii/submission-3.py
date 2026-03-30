e05b6575fbd10f333ee076dc2315e133d75e8bb8
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-03-30 02:53:38 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s, l, r):
+        while l<r:
+            if s[l] != s[r]:
+                return False
+            l, r = l+1, r-1
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left+1, right) or self.isPalindrome(s, left, right-1)
+            left, right = left +1, right -1
+        return True
+        
