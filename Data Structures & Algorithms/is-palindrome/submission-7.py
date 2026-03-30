2afc8928aa6028b5e00d1a251ab1ff63591716f7
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-03-30 02:53:29 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isalphanum(self, c):
+        return ( ord('A') <= ord(c) <= ord('Z') or
+                 ord('a') <= ord(c) <= ord('z') or
+                 ord('0') <= ord(c) <= ord('9'))
+
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) -1
+        while left < right:
+
+            while left < right and not self.isalphanum(s[left]):
+                left += 1
+            while right > left and not self.isalphanum(s[right]):
+                right -= 1
+                
+            if s[left].lower() != s[right].lower():
+                return False
+            left, right = left + 1, right - 1
+        return True
