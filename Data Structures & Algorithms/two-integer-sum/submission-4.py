42f34c8127f5b336860e4d4ea730b2fede9c8599
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-04-01 14:03:12 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        count = defaultdict(int)
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in count:
+                return [count[diff], i]
+            count[num] = i
