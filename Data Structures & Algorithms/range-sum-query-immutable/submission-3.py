55804ef36751a9701749abbf4b541ce40ba03ff0
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-04-02 20:03:23 -0400

    Add: range-sum-query-immutable - submission-3

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-3.py
@@ -0,0 +1,23 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix_arr = nums.copy()
+        # total = 0
+        # for num in nums:
+        #     total += num
+        #     self.prefix_arr.append(total)
+    def sumRange(self, left: int, right: int) -> int:
+        # right_sum = self.prefix_arr[right]
+        # if left > 0:
+        #     left_sum = self.prefix_arr[left - 1]
+        # else:
+        #     left_sum = 0
+        # return right_sum - left_sum
+
+        arr = self.prefix_arr[left : right+ 1]
+        total = 0
+        for num in arr:
+            total += num
+        return total
+        # print(arr)
+
