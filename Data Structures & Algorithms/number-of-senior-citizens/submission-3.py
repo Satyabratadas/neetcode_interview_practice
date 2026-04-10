abed4347c7bab1bc1991d2f84bac8652b3d98bac
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-04-10 17:17:06 -0400

    Add: number-of-senior-citizens - submission-3

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for passenger in details:
+            for i, ch in enumerate(passenger):
+                current_age = ""
+                if ord("A") <= ord(ch) <= ord("Z"):
+                    for j in range(i+1, i +3):
+                        current_age += passenger[j]
+                    if int(current_age) > 60:
+                        count += 1
+        return count
+
+
+        
+        
