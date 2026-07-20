b150570d775afcd90e5757cd9d9ad3884e6d2dcf
Satyabrata13 <62879698+Satyabratadas@users.noreply.github.com>
2026-07-20 18:27:28 -0400

    Add: arranging-coins - submission-9

---
+++ Data Structures & Algorithms/arranging-coins/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left = 0
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            coins = (mid * (mid + 1)) // 2
+            if coins <= n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+        
